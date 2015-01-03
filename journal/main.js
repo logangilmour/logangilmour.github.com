c2bec33e8e4acf5912022c4f5a5efe3160f71b00
logangilmour <logangilmour@Logans-MacBook-Air-3.local>
2015-01-03 11:33:23 -0700

    Add day

--- journal/main.js
+++ journal/main.js
@@ -8,9 +8,30 @@ function formatDate(date){
 date.getFullYear();
 }
 
+function lifts(){
+    $('squat, benchpress, deadlift, powerclean').each(function(e){
+	var parts = $(e).text().split("x");
+	parts[2] = parts[2]*2;
+	$(e).text(parts[0]+"x"+parts[1]+"x"+parts[2]);
+    });
+}
+
+function bodyfat(){
+    $('bodyfat').each(function(i,e){
+	console.log($(e).text());
+	$(e).text(_.reduce($(e).text().split(/\s+/),function(a,b){
+	    return parseInt(b)+a;
+	},0) + "mm");
+    });
+
+}
+
 $(function(){
 $('article>time:first-child').each(function(i,e){
     var date = new Date(Date.parse($(e).attr('datetime')));
    $(e).text(formatDate(date));
+   
 });
+    lifts();
+    bodyfat();
 });
